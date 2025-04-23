211c84cee9d7ff5c5697684f81fc15be677a6396
AbdulSaboor393 <yousufsaboor@gmail.com>
2025-04-23 21:55:57 +0500

    Update Task 2.cpp

--- lab tasks/Lab 9/Task 2.cpp
+++ lab tasks/Lab 9/Task 2.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 class MenuItem {
@@ -7,14 +9,15 @@ protected:
     double price;
 
 public:
-    MenuItem(string n, double p) : dishName(n), price(p) {}
+    MenuItem(const string& n, double p) : dishName(n), price(p) {}
+    virtual ~MenuItem() {} // Virtual destructor for proper cleanup
     virtual void showDetails() = 0;
     virtual void prepare() = 0;
 };
 
 class Appetizer : public MenuItem {
 public:
-    Appetizer(string n, double p) : MenuItem(n, p) {}
+    Appetizer(const string& n, double p) : MenuItem(n, p) {}
 
     void showDetails() override {
         cout << "Appetizer: " << dishName << " - $" << price << endl;
@@ -27,7 +30,7 @@ public:
 
 class MainCourse : public MenuItem {
 public:
-    MainCourse(string n, double p) : MenuItem(n, p) {}
+    MainCourse(const string& n, double p) : MenuItem(n, p) {}
 
     void showDetails() override {
         cout << "Main Course: " << dishName << " - $" << price << endl;
@@ -38,19 +41,38 @@ public:
     }
 };
 
+class Dessert : public MenuItem {
+public:
+    Dessert(const string& n, double p) : MenuItem(n, p) {}
+
+    void showDetails() override {
+        cout << "Dessert: " << dishName << " - $" << price << endl;
+    }
+
+    void prepare() override {
+        cout << "Plating dessert: " << dishName << endl;
+    }
+};
+
 int main() {
-    MenuItem* m1 = new Appetizer("Chicken Strips", 4.99);
-    MenuItem* m2 = new MainCourse("Alfredo Pasta", 22.50);
+    MenuItem* m1 = new Appetizer("Garlic Bread", 6.99);
+    MenuItem* m2 = new MainCourse("Mutton Ribs", 22.50);
+    MenuItem* m3 = new Dessert("Chocolate Lava Cake", 6.75);
 
     m1->showDetails();
     m1->prepare();
-    cout << "\n"<<endl;
+    cout << endl;
 
     m2->showDetails();
     m2->prepare();
+    cout << endl;
+
+    m3->showDetails();
+    m3->prepare();
 
     delete m1;
     delete m2;
+    delete m3;
 
     return 0;
 }
